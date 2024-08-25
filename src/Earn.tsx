7c298d2ec465d71fc93cd2cd6550bb6935addc8e
syedanastanweer <syedanastanwer@gmail.com>
2024-08-25 21:18:35 +0500

    commit15

--- src/Earn.tsx
+++ src/Earn.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect } from 'react';
 import { useParams, useNavigate } from 'react-router-dom';
 
 const Earn: React.FC = () => {
